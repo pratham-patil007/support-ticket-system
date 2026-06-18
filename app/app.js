02e65d72e496c4ec17e9bfd395f79b0aab601b55
Pratham Patil <your-email@gmail.com>
2026-06-18 10:00:39 +0530

    Ready for deployment

---
+++ app/app.js
@@ -0,0 +1,124 @@
+const API = "http://127.0.0.1:8000";
+
+function showSection(id){
+
+    document
+        .querySelectorAll(".section")
+        .forEach(section=>{
+            section.classList.remove("active");
+        });
+
+    document
+        .getElementById(id)
+        .classList.add("active");
+}
+
+async function registerUser(){
+
+    const response = await fetch(`${API}/register`,{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+
+            name:rname.value,
+            email:remail.value,
+            password:rpassword.value
+
+        })
+    });
+
+    const data = await response.json();
+
+    alert(JSON.stringify(data));
+}
+
+async function loginUser(){
+
+    alert("Connect Login API Here");
+}
+
+async function createTicket(){
+
+    const response = await fetch(`${API}/tickets`,{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+
+            title:title.value,
+            description:description.value,
+            priority:priority.value
+
+        })
+    });
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    loadTickets();
+}
+
+async function loadTickets(){
+
+    const response = await fetch(`${API}/tickets`);
+
+    const tickets = await response.json();
+
+    document.getElementById("totalTickets").innerText =
+        tickets.length;
+
+    let html="";
+
+    tickets.forEach(ticket=>{
+
+        html += `
+        <tr>
+
+            <td>${ticket.id}</td>
+
+            <td>${ticket.title}</td>
+
+            <td>${ticket.priority}</td>
+
+            <td>
+
+                <button
+                    class="btn btn-danger btn-sm"
+                    onclick="deleteTicket(${ticket.id})">
+
+                    Delete
+
+                </button>
+
+            </td>
+
+        </tr>
+        `;
+    });
+
+    document.getElementById("ticketTable").innerHTML =
+        html;
+}
+
+async function deleteTicket(id){
+
+    await fetch(`${API}/tickets/${id}`,{
+        method:"DELETE"
+    });
+
+    loadTickets();
+}
+
+showSection("dashboard");
+
+loadTickets();
--- app/main.py
+++ app/main.py
@@ -7,9 +7,20 @@ from app.routes import ticket_routes
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 app.include_router(ticket_routes.router)
 app.include_router(auth_routes.router)
 
 @app.get("/")
 def home():
     return {"message": "API Running"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
